d96205e8156891021b1c4424a7671fe96224414a
joergior <78919242+joergior@users.noreply.github.com>
2021-05-10 18:03:23 +0200

    ok

--- esercitazione.py
+++ esercitazione.py
@@ -8,6 +8,16 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import wordnet as wn
 from nltk.stem import WordNetLemmatizer
 
+def annotated_data():
+    file = open("framenet_annotation.txt", "r")
+    dict = {}
+    for line in file:
+        data = line.split(",")
+        s = data[1]
+        _s = s.replace('\n','')
+        dict[data[0].lower()] = _s
+    return dict
+
 
 def get_sentence_list(s):
     elements = s.split('_')
@@ -84,21 +94,39 @@ def fn_disambiguation_context(f,r):
     return s_max
 
 
-
-
-
 ids = getFrameSetForStudent('Ramundo')
+synsets = {}
 for i in ids:
+    list = []
     f = fn.frame_by_id(i)
     print('*********SYNSETS FOR {}************'.format(f.name))
     sentence = str(f.name).split('_')
     r = reggente(sentence)
     print('Synset for {} is {}'.format(r,fn_disambiguation_context(f,r)))
+    list.append((r.lower(),fn_disambiguation_context(f,r)))
     FEs = f.FE.keys()
     for fe in FEs:
         fes = fe.replace('_', ' ')
         print('Synset for {} is {}'.format(fes,fn_disambiguation_context(f, fes)))
+        list.append((fes.lower(),fn_disambiguation_context(f, fes)))
     LUs = f.lexUnit.keys()
     for lu in LUs:
         lus = lu.split('.')
         print('Synset for {} is {}'.format(lus[0],fn_disambiguation_context(f, lus[0])))
+        list.append((lus[0].lower(),fn_disambiguation_context(f, lus[0])))
+    synsets[i]=list
+data = annotated_data()
+c = 0
+n = 0
+for key in synsets:
+    n = n + len(synsets[key])
+    for i in range(len(synsets[key])):
+        w = synsets[key][i][0]
+        set = synsets[key][i][1]
+        try:
+            _set = data[w]
+        except:
+            continue
+        if data[w] == str(set):
+            c = c + 1
+print('Accuracy: ' + str(c/n))
